b84d54b9bd6af40d297e162be45ab260492a2272
SunSof <buketovasofi@gmail.com>
2019-12-08 19:26:57 +0300

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -376,7 +376,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51578" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49585" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
