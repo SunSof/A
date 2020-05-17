d2bd48b8460d894f62efc946f3d8bce24e050404
SunSof <buketovasofi@gmail.com>
2020-05-17 17:43:25 +0300

    fix

--- src/scroll.js
+++ src/scroll.js
@@ -1,4 +1,5 @@
 // скролл от стартовой страницы до "о нас" Jquery
+
 $(".b-start__arrow").on("click", function(event) {
   var target = $(".b-about");
 
@@ -14,6 +15,29 @@ $(".b-start__arrow").on("click", function(event) {
       );
   }
 });
+
+
+
+// $(".b-start").on("click", function(event) {
+//   let target = event.target;
+//   let hello = document.querySelector(".b-hello");
+//   console.log(target);
+//   if (target.classList.contans("b-start__btn-hello")) {
+//     if (hello.length) {
+//       event.preventDefault();
+//       $("html, body")
+//         .stop()
+//         .animate(
+//           {
+//             scrollTop: target.offset().top
+//           },
+//           1000
+//         );
+//     }
+//   }
+// });
+
+
 
 // $(".b-start__btn").on('click', function(event) {
 
